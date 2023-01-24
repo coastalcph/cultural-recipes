8a6c522a84528135adfa6fee2a3a596f9cf055b1
Yong <90298102+yongcaoplus@users.noreply.github.com>
2023-01-24 21:15:04 +0100

    Update eval_seq2seq.py

--- adaptation_baseline/eval/eval_seq2seq.py
+++ adaptation_baseline/eval/eval_seq2seq.py
@@ -18,23 +18,23 @@ class Evaluator:
         if target_lan == "en":
             return self.bleu.compute(predictions=preds, references=labels)['bleu']
         elif target_lan == "cn":
-            return self.bleu.compute(predictions=[' '.join(list(p)) for p in preds], 
+            return self.bleu.compute(predictions=[' '.join(list(p)) for p in preds],
                                      references=[[' '.join(list(l)) for l in label] for label in labels])['bleu']
 
     def get_roughl(self, preds, labels):
         if self.language == "en":
             return self.rougeScore.compute(predictions=[preds], references=[labels])['rougeLsum']
         elif self.language == "cn":
-            return self.rougeScore.compute(predictions=' '.join(list(preds)),
+            return self.rougeScore.compute(predictions=[' '.join(list(preds))],
                                            references=[[' '.join(list(item)) for item in labels]])['rougeLsum']
 
     def get_bert_score(self, preds, labels):
         if self.language == "en":
-            return self.bertScore.compute(predictions=preds, references=labels, 
-                                               model_type='bert-base-uncased', lang='en', verbose=True)['f1']
+            return self.bertScore.compute(predictions=preds, references=labels,
+                                          model_type='bert-base-uncased', lang='en', verbose=True)['f1']
         elif self.language == "cn":
-            return self.bertScore.compute(predictions=preds, references=labels, 
-                                               model_type='bert-base-chinese', lang='cn', verbose=True)['f1']
+            return self.bertScore.compute(predictions=preds, references=labels,
+                                          model_type='bert-base-chinese', lang='cn', verbose=True)['f1']
 
 
 def evaluate_all(path, evaluator):
@@ -60,7 +60,7 @@ def evaluate_all(path, evaluator):
 
 def extract_componets(content, fields):
     # return content
-    components = re.split("Title:|title:|ingredients:|Ingredients:", content)
+    components = re.split("标题|Title:|title:|ingredients:|Ingredients:", content)
     # breakpoint()
     # title + ingre + step
     if len(components) > 2:
@@ -107,6 +107,7 @@ def evaluate_component(path, evaluator, fields):
             recover = extract_componets(json.loads(row)['prediction'].strip(), fields)
             recover = recover.replace(args.eos, '').replace(args.sos, '').replace(args.sep, '').replace(args.pad, '')
             avg_len.append(len(recover.split(' ')))
+            # breakpoint()
             try:
                 rougel.append(evaluator.get_roughl(recover, reference))
             except:
@@ -114,6 +115,7 @@ def evaluate_component(path, evaluator, fields):
             recovers.append(recover)
             references.append(reference)
             cnt += 1
+    breakpoint()
     bleu = evaluator.get_corpus_bleu(recovers, references, target_lan)
     bert_score = evaluator.get_bert_score(recovers, references)
 
@@ -133,6 +135,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     path = args.file
+    print("we are evaluating ", path)
     evaluator = Evaluator(args.tgt_lan)
     target_lan = args.tgt_lan
     component = args.component
@@ -140,8 +143,8 @@ if __name__ == '__main__':
         bleu, rougel, F1, avg_len = evaluate_all(path, evaluator)
     else:
         bleu, rougel, F1, avg_len = evaluate_component(path, evaluator, component)
-    
-    print('*'*30)
+
+    print('*' * 30)
     print('BLEU score', bleu)
     print('avg ROUGE-L score', np.mean(rougel))
     print('avg berscore', np.mean(F1))

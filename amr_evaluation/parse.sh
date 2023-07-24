a73f4642a02c75c7e88eabac31d2f3bde83a89c6
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2023-07-24 15:29:22 +0200

    Script to run XAMR
    Currently fails due to
    RuntimeError: Address already in use

--- amr_evaluation/parse.sh
+++ amr_evaluation/parse.sh
@@ -1,26 +1,29 @@
-#!/bin/bash
+#!/bin/bash -x
 #SBATCH --job-name=parse
-#SBATCH --partition=cpu
-#SBATCH --ntasks=4
+#SBATCH -p gpu --gres=gpu:1
+#SBATCH --ntasks=4 --cpus-per-task=4
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=16GB
 #SBATCH --output=parse.out
-#SBATCH --time=00:01:00
+#SBATCH --time=08:00:00
 
-checkpoint=ckpt/best.pt # this points to the pretrained model you have downloaded
+echo $SLURMD_NODENAME $CUDA_VISIBLE_DEVICES
+. /etc/profile.d/modules.sh
+eval "$(conda shell.bash hook)"
+conda activate ~/anaconda3/envs/xamr
+checkpoint=XAMR/ckpt/best.pt # this points to the pretrained model you have downloaded
+indir=/ceph/hpc/data/d2023d06-049-users/xamr/input
+outdir=/ceph/hpc/data/d2023d06-049-users/xamr/pred
+mkdir -p $outdir
 
-dataset=tmp.txt # this points to the data you want to parse (see detailed explanation below)
-python prediction2txt.py sampled_human_gpt4_cn2en.jsonl $dataset en
-
-for jsonl in ../../data/generation_results/human_cn2en/*.jsonl; do
-	python prediction2txt.py $jsonl $dataset en
-	PYTHONPATH=. python3 bin/predict_amrs.py \
+for dataset in $indir/*.txt; do
+	PYTHONPATH=XAMR python3 XAMR/bin/predict_amrs.py \
 	   --model facebook/mbart-large-50-many-to-many-mmt \
 	   --checkpoint ${checkpoint} \
 	   --dataset ${dataset} \
 	   --nproc-per-node 4 \
-	   --gold-path tmp-gold.txt \
-	   --pred-path tmp-pred.txt \
+	   --gold-path $outdir/$(basename $dataset txt)gold.txt \
+	   --pred-path $outdir/$(basename $dataset txt)pred.txt \
 	   --beam-size 4 \
 	   --batch-size 5000 \
 	   --penman-linearization \

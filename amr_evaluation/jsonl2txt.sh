9c2a0f422e45cf77a8f9c97d7e401edf886b51c1
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2023-07-24 15:28:51 +0200

    Script to convert jsonl outputs to input files for XAMR

--- amr_evaluation/jsonl2txt.sh
+++ amr_evaluation/jsonl2txt.sh
@@ -7,13 +7,15 @@
 #SBATCH --output=parse.out
 #SBATCH --time=00:01:00
 
-python source2txt.py ../data/generation_results/human_cn2en/sampled_human_gpt4_cn2en.jsonl cn2en_source.txt zh
-python reference2txt.py ../data/generation_results/human_cn2en/sampled_human_gpt4_cn2en.jsonl cn2en_reference en
+outdir=/ceph/hpc/data/d2023d06-049-users/xamr/input
+mkdir -p $outdir
+python source2txt.py ../data/generation_results/human_cn2en/sampled_human_gpt4_cn2en.jsonl $outdir/cn2en_source.txt zh
+python reference2txt.py ../data/generation_results/human_cn2en/sampled_human_gpt4_cn2en.jsonl $outdir/cn2en_reference.txt en
 for jsonl in ../data/generation_results/human_cn2en/*.jsonl; do
-	python prediction2txt.py $jsonl $(basename $jsonl jsonl).txt en
+	python prediction2txt.py $jsonl $outdir/$(basename $jsonl jsonl)txt en
 done
-python source2txt.py ../data/generation_results/human_en2cn/sampled_human_gpt4_en2cn.jsonl en2cn_source.txt en
-python reference2txt.py ../data/generation_results/human_en2cn/sampled_human_gpt4_en2cn.jsonl en2cn_reference zh
+python source2txt.py ../data/generation_results/human_en2cn/sampled_human_gpt4_en2cn.jsonl $outdir/en2cn_source.txt en
+python reference2txt.py ../data/generation_results/human_en2cn/sampled_human_gpt4_en2cn.jsonl $outdir/en2cn_reference.txt zh
 for jsonl in ../data/generation_results/human_en2cn/*.jsonl; do
-	python prediction2txt.py $jsonl $(basename $jsonl jsonl).txt zh
+	python prediction2txt.py $jsonl $outdir/$(basename $jsonl jsonl)txt zh
 done
